1f1ada2c0cba09f1ef5da95e6162bdeb8e2dfd20
Winter Snowfall <WinterSnowfall@users.noreply.github.com>
2020-11-07 12:25:51 +0200

    Delete boincmon_start.sh

--- scripts/boincmon_start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -sp "Please enter master password: " password
-echo
-echo $password | nohup ./pi_boincmon.py 1>/dev/null 2>&1 &
-sleep 2s
-echo "Service started - now tailing logs. Press CTRL + C to exit."
-tail -f ../logs/pi_boincmon_service.log
-
